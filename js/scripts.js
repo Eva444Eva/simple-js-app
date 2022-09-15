029f01e6fe31413d200515a4c514cf45923b1a02
Eva444Eva <michaela.proios@gmail.com>
2022-09-15 17:38:34 +0200

    added first array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favouriteFood = "pancakes";
-document.write(favouriteFood);
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  { name: 'Rattata', height: 0.3, types: ['normal']},
+  { name: 'Slowbro', height: 1.6, types: ['psychic', 'water']},
+];
