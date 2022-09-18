07f84c164688409eac6f4fd78d6db5e3079c3736
Eva444Eva <michaela.proios@gmail.com>
2022-09-18 21:51:06 +0200

    added two loops

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,19 @@ let pokemonList = [
   { name: 'Rattata', height: 0.3, types: ['normal']},
   { name: 'Slowbro', height: 1.6, types: ['psychic', 'water']},
 ];
+
+// this loop writes all the Pokemon names in the array with height
+document.write('<ul>');
+for (let i = 0, pl = pokemonList.length; i < pl; i++) {
+  document.write('<li class="list-item">');
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')');
+
+  // Sentence below appears when Pokemon is higher that 1
+  if (pokemonList[i].height >= 1) {
+    document.write(" - Wow, that is big!");
+  }
+  document.write('</li>');
+}
+document.write('</ul>');
+
+
