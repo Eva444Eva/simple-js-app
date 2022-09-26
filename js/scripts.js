bf5ec0950c2dfd9d037c932a2a7fe883fac38ecf
Eva444Eva <michaela.proios@gmail.com>
2022-09-26 15:14:35 +0200

    added AddListItem function, showDetails function (incl. eventListener)

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ let pokemonRepo = (function() {
     { name: 'Rattata', height: 0.3, types: ['normal']},
     { name: 'Slowbro', height: 1.6, types: ['psychic', 'water']},
   ];
+  const requiredPokemonParams = ['name', 'height', 'types'];
 
   function getAll() {
     return pokemonList;
@@ -12,7 +13,7 @@ let pokemonRepo = (function() {
 
   // define separate function add(item)
   function add(item) {
-    if (typeof item === 'object') {
+    if (typeof item === 'object' && requiredPokemonParams.every(key => Object.keys(item).includes(key))) {
       pokemonList.push(item);
     } else {
       // do nothing
@@ -26,31 +27,40 @@ let pokemonRepo = (function() {
     return result; 
   }
 
+  function addListItem(pokemon) {
+    const pokemonListElement = document.querySelector(".pokemon-list");
+    const pokemonElement = document.createElement("li");
+    const button = document.createElement("button");
+
+    pokemonListElement.appendChild(pokemonElement);
+
+    pokemonElement.classList.add('list-item');
+    pokemonElement.appendChild(button);
+
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+
+    button.addEventListener('click', (event) => {
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
   // return object with the new public functions assigned as keys
   return {
     getAll: getAll,
     add: add,
-    findByName: findByName
+    findByName: findByName,
+    addListItem: addListItem
   };
 })();
 
-//this loop writes all the Pokemon names in the array with height
-document.write('<ul class="list">');
+pokemonRepo.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
 
-pokemonRepo.getAll().forEach(pokemon => {
-  // opening tag
-  document.write('<li class="list-item">');
-
-  // main info
-  document.write(pokemon.name + ': ' + ' height -  '+ pokemon.height);
-  
-  // conditional note
-  if (pokemon.height >= 1) {
-    document.write(' - Wow, that is big!');
-  }
-
-  // closing tag
-  document.write('</li>');
-}); 
-
-document.write('</ul>');
+pokemonRepo.getAll().forEach(function(pokemon) {
+  console.log(pokemon);
+  pokemonRepo.addListItem(pokemon);
+});
